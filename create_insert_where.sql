30879329b69a7bbe54b3bc7d6c1e42549d53d46b
laksh@Priya <lakshmipriyapalla1@gmail.com>
2026-04-24 20:16:49 +0530

    Added SQL queries (CREATE, INSERT, SELECT, WHERE)

---
+++ create_insert_where.sql
@@ -0,0 +1,140 @@
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+
+show databases;
+use amazon_db;
+create table users(
+userid int auto_increment primary key,
+name varchar(10) not null,
+email varchar(20) unique not null,
+phno varchar(10) unique,
+pwd varchar(7),
+location varchar(10));
+select * from users;
+insert into users(name,email,phno,pwd,location)
+values('lakshmi','lakshmi@gmail.com','9848943020','rtyuio','addanki');
+insert into users(name,email,phno,pwd,location)
+values('teja','teja@gmail.com','996793414','kjhgf','addanki'),
+('pandu','pandu@gmail.com','9876542345','mnbvc','hyd'),
+('anuradha','radha@gmail.com','7654322389','xcvb','hyd');
+select * from users where location='addanki';
+select name,email from users;
+select *from products;
+
+
+
